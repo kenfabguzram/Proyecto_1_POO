7e597d240ee66d651638a139cc186dd53b112f25
kenfabguzram <95146991+kenfabguzram@users.noreply.github.com>
2022-04-24 15:02:48 -0600

    ADD Interfaz y Mapa
    Se agregaron la interfaz gráfica y el mapa

---
+++ Proyecto_1/src/main/Controlador/main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_1_version_kendall;
+
+/**
+ *
+ * @author Kendall_Guzmán
+ */
+public class ClaseInicial {
+    
+}
